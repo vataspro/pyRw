0b0c710289b3c7931b515152a8095138fe64637a
Alexis Provatas <vataspro@gmail.com>
2025-08-06 18:01:16 +0100

    bug and style fix

--- pyRw/utils.py
+++ pyRw/utils.py
@@ -15,12 +15,17 @@ def ensureValidObservableShape(x):
     if not ((type(x) is list) or ((type(x) is np.ndarray) and len(x.shape) == 2)):
         raise ValueError("Passed object is not a list or 2d array")
 
-    if not all(
-        [   # if it contains anything that isn't a list of lists and/or arrays
-            (type(x_) is list) or ((type(x_) is np.ndarray) and (len(x_.shape) == 1))
-            for x_ in x
-        ]
-    ) or (len(x) == 0) or all([len(x_) == 0 for x_ in x]):
+    if (
+        not all(
+            [  # if it contains anything that isn't a list of lists and/or arrays
+                (type(x_) is list)
+                or ((type(x_) is np.ndarray) and (len(x_.shape) == 1))
+                for x_ in x
+            ]
+        )
+        or (len(x) == 0)
+        or all([len(x_) == 0 for x_ in x])
+    ):
         raise ValueError(
             "Passed object is neither a list of lists or a list of 1d arrays"
         )
@@ -38,7 +43,6 @@ def checkObservableNotNegative(x):
 
 # Ensure that two lists of lists are the same shape
 def ensureSameShape2d(a, b):
-    ensureValidObservableShape(a, b)
     if not same_shape2d(a, b):
         raise ValueError("The Observable is not the same shape as the actions")
 
@@ -60,7 +64,7 @@ def binObservable(Q, binSizes):
 
     return [bin1d(q, binsize) for (q, binsize) in zip(Q, binSizes)]
 
+
 # 1d binning
 def bin1d(q, binsize):
     return q[::binsize]
-
